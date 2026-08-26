63ca313d4de14e0b639d0fd9dbcd3aaee417772f
FMB2K9 <124444097+FMB2K9@users.noreply.github.com>
2026-08-26 13:38:58 +0100

    refactor: enhance Firebase config handling to use fallback values from environment variables

--- netlify/functions/get-firebase-config.js
+++ netlify/functions/get-firebase-config.js
@@ -1,4 +1,3 @@
-exports.handler = async function() {
 /**
  * Netlify Function: get-firebase-config
  * 
@@ -17,21 +16,14 @@ exports.handler = async function(event) {
 
     // Construct the Firebase config object from environment variables.
     const firebaseConfig = {
-        apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.PUBLIC_FIREBASE_DATABASE_URL,
-        projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.PUBLIC_FIREBASE_APP_ID,
+        apiKey: process.env.PUBLIC_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY,
+        authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.PUBLIC_FIREBASE_DATABASE_URL || process.env.DATABASE_URL,
+        projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET || process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID || process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.PUBLIC_FIREBASE_APP_ID || process.env.FIREBASE_APP_ID,
         measurementId: process.env.PUBLIC_FIREBASE_MEASUREMENT_ID
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
     };
 
     return {
